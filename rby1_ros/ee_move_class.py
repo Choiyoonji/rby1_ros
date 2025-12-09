Fix: update quaternion math and adjust rotation duration in Move_ee and Rotate_ee classes

rby1_ros/ee_move_class.py
@@ -1,9 +1,9 @@
-from trajectory import Trajectory
+from .trajectory import Trajectory
 import numpy as np
 from typing import List, Tuple, Union
 import time
 from matplotlib import pyplot as plt
-from utils import mul_quat_xyzw, mul_quat, quat_diff_xyzw, normalize_quat
+from .utils import mul_quat_xyzw, mul_quat, quat_diff_xyzw, normalize_quat
 
 class Move_ee:
     def __init__(self, Hz=100, duration=2.0, dist_step=0.01):
@@ -204,7 +204,7 @@ class Rotate_ee:
         for step in range(1, self.total_step + 1):
             current_time = step / self.Hz
             quat, quat_vel, quat_acc = self.trajectory_ee_quat.calculate_pva_quat(current_time)
-            quat_rel = quat_diff_xyzw(prev_quat, quat)
+            quat_rel = normalize_quat(quat_diff_xyzw(prev_quat, quat))
             w_val = np.clip(quat_rel[3], -1.0, 1.0)
             theta_diff = 2.0 * np.arccos(w_val)
             

rby1_ros/rby1_impedance_control_command.py
@@ -549,7 +549,7 @@ class RBY1Node(Node):
                     .set_command_header(rby.CommandHeaderBuilder().set_control_hold_time(self.settings.dt * 10))
                     .set_minimum_time(Settings.dt * 1.02)
                     .set_joint_stiffness([320, 320, 320, 200, 200, 200, 200])
-                    .set_joint_torque_limit([40, 40, 40, 30, 30, 30, 30])
+                    .set_joint_torque_limit([80, 80, 80, 60, 60, 60, 60])
                     .add_joint_limit("right_arm_3", -2.6, -0.5)
                     # .add_joint_limit("right_arm_5", 0.2, 1.9)
                     .set_nullspace_joint_target(np.deg2rad([0.0, -15.0, 0.0, -120.0, 0.0, 40.0, -15.0]), np.array([1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.5]) * 2, 0.2, 0.3)